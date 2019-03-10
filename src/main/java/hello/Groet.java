6ce5cd25e4df30ba765c88fe1bb637448bb00950
Sajaki <Sajaki9@gmail.com>
2019-03-10 21:22:35 +0100

    init

---
+++ src/main/java/hello/Groet.java
@@ -0,0 +1,11 @@
+package hello;
+
+public class Groet {
+    /*
+       functie in java
+     */
+    public String sayHello()
+    {
+        return "Hello world!";
+    }
+}
---
+++ src/main/java/hello/Hallo.java
@@ -0,0 +1,11 @@
+package hello;
+
+public class Hallo {
+
+    public static void main(String[] args) {
+        Groet greeter = new Groet();
+        System.out.println(greeter.sayHello());
+    }
+
+
+}
